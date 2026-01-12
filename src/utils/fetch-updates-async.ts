939a71496e48984a9cea3030966af7ada43675ba
Maxime-NS <maxime.gey@nativesquare.fr>
2026-01-13 00:22:04 +0100

    feat: updates

---
+++ src/utils/fetch-updates-async.ts
@@ -0,0 +1,15 @@
+import * as Updates from "expo-updates";
+
+export async function fetchUpdatesAsync() {
+  try {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    // You can also add an alert() to see the error message in case of an error when fetching updates.
+    alert(`Error fetching latest Expo update: ${error}`);
+  }
+}
